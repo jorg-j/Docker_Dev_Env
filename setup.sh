8e9af03c7889f765b94b308bf9e75790375efbbd
Jack <jorgensen.jack@gmail.com>
2023-06-17 09:44:39 +1000

    update

--- setup.sh
+++ setup.sh
@@ -1,7 +1,14 @@
-
-
 wget https://github.com/jorg-j/Docker_Dev_Env/archive/main.zip
 unzip main.zip
+
+directory="Docker_Dev_Env-main"
+
+while [ ! -d "$directory" ]; do
+  echo "Waiting for directory to exist..."
+  sleep 1 
+done
+
 rm main.zip
-mv -r Docker_Dev_Env-main/* .
-rm -r Docker_Dev_Env-main
+mv -r $directory/ .
+rm -r $directory
+rm -r .git
